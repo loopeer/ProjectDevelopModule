f3a3c5bd1527fd0466c522011cae4ddb869a210b
ToDou <yytodou123@gmail.com>
2016-10-19 15:13:35 +0800

    add first src

---
+++ app/src/main/java/loopeer/com/projectdevelopmodule/MainActivity.java
@@ -0,0 +1,13 @@
+package loopeer.com.projectdevelopmodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

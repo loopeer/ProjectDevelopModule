60db159cd327fda2bf7c096558a84ef047b03d0c
lzz <liuzizhang1994@gmail.com>
2016-11-25 13:54:27 +0800

    add ValidatorUtils

---
+++ library/developutils/src/main/java/com.loopeer.developutils/ValidatorUtils.java
@@ -0,0 +1,30 @@
+package com.loopeer.developutils;
+
+public final class ValidatorUtils {
+
+    public static final String REGEX_PHONE = "^1[0-9]{10}$";
+
+    public static final String REGEX_EMAIL
+        = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
+    public static final String REGEX_ID_CARD = "(^\\d{15}$)|(\\d{17}(?:\\d|x|X)$)";
+
+    private ValidatorUtils(){}
+
+    public static boolean isPhone(String phone){
+        return phone.trim().matches(REGEX_PHONE);
+    }
+
+    public static boolean isEmail(String email){
+        return email.trim().matches(REGEX_EMAIL);
+    }
+
+    /**
+     * the regex actually is not correct , but is enough for most situation
+     * @param IDCard ID Card number
+     * @return true if the ID Card number is legal;
+     */
+    public static boolean isIDCard(String IDCard){
+        return IDCard.trim().matches(REGEX_ID_CARD);
+    }
+}
